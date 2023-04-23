c50ea44096d49db7d1e1369cfc202c8320068136
Prashanth Sriram <prashanthsriram145@gmail.com>
2023-04-23 12:41:28 +0530

    Initial commit

---
+++ src/test/java/com/prashanth/poc/graphql/PocGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prashanth.poc.graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
